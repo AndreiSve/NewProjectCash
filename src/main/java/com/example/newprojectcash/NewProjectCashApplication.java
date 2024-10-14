5b298cc2749ee4c9781594e631caa0612743555a
AndreiSve <aasvekolnikov@gmail.com>
2024-10-14 19:54:16 +0300

    Initial commit

---
+++ src/main/java/com/example/newprojectcash/NewProjectCashApplication.java
@@ -0,0 +1,13 @@
+package com.example.newprojectcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProjectCashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewProjectCashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newprojectcash/NewProjectCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newprojectcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjectCashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
